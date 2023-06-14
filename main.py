662ee5e70a90cb8050cc901eb85c7f640d97252a
DJW <1685727864@qq.com>
2023-06-14 17:25:17 +0800

    注释掉非必要静态文件，在某些平台会出现错误

--- main.py
+++ main.py
@@ -31,26 +31,26 @@ app.add_middleware(
 )
 
 
-# 重写 swagger_ui 接口文档设置，使文档能够在局域网访问
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - Swagger UI",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="/swagger-ui-bundle.js",
-        swagger_css_url="/swagger-ui.css",
-    )
-
-
-# 重写 redoc 接口文档设置，使文档能够在局域网访问
-@app.get("/redoc", include_in_schema=False)
-async def redoc_html():
-    return get_redoc_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - ReDoc",
-        redoc_js_url="/redoc.standalone.js",
-    )
+# # 重写 swagger_ui 接口文档设置，使文档能够在局域网访问
+# @app.get("/docs", include_in_schema=False)
+# async def custom_swagger_ui_html():
+#     return get_swagger_ui_html(
+#         openapi_url=app.openapi_url,
+#         title=app.title + " - Swagger UI",
+#         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+#         swagger_js_url="/swagger-ui-bundle.js",
+#         swagger_css_url="/swagger-ui.css",
+#     )
+#
+#
+# # 重写 redoc 接口文档设置，使文档能够在局域网访问
+# @app.get("/redoc", include_in_schema=False)
+# async def redoc_html():
+#     return get_redoc_html(
+#         openapi_url=app.openapi_url,
+#         title=app.title + " - ReDoc",
+#         redoc_js_url="/redoc.standalone.js",
+#     )
 
 # 事件监听
 app.add_event_handler("startup", Events.startup(app))       # fastapi启动时触发，包含注册数据库、redis等操作
@@ -58,8 +58,8 @@ app.add_event_handler("shutdown", Events.stopping(app))     # fastapi关闭时�
 # 路由
 app.include_router(Router.router)   # 包含各个路由
 # 设置静态资源目录
-app.mount('/', StaticFiles(directory=settings.STATIC_DIR), name="static")
-app.state.views = Jinja2Templates(directory=settings.TEMPLATE_DIR)
+# app.mount('/', StaticFiles(directory=settings.STATIC_DIR), name="static")
+# app.state.views = Jinja2Templates(directory=settings.TEMPLATE_DIR)
 
 if __name__ == '__main__':
     uvicorn.run(
